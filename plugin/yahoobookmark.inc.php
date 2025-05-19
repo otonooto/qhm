92a8644e1e6fde1bca8decf21eaf2dde0592e8c6
otonooto <210685095+otonooto@users.noreply.github.com>
2025-05-19 13:30:56 +0900

    fix typo

--- plugin/yahoobookmark.inc.php
+++ plugin/yahoobookmark.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *   Yahoo Bookmark Plugin v1.1
  *   -------------------------------------------
@@ -24,54 +25,40 @@ function plugin_yahoobookmark_inline()
 {
 	$args = func_get_args();
 	$text = $short = $nowindow = FALSE;
-	
-	foreach ($args as $arg)
-	{
+	$btn_height = '';
+
+	foreach ($args as $arg) {
 		$arg = trim($arg);
-		if ($arg === 'short' && $short === FALSE)
-		{
+		if ($arg === 'short' && $short === FALSE) {
 			$short = TRUE;
-		}
-		else if ($arg === 'nowindow' && $nowindow === FALSE)
-		{
+		} else if ($arg === 'nowindow' && $nowindow === FALSE) {
 			$nowindow = TRUE;
-		}
-		else if (trim($arg) !== '')
-		{
+		} else if (trim($arg) !== '') {
 			$text = $arg;
 		}
 	}
 
-	if ($text === FALSE OR trim($text) === '')
-	{
+	if ($text === FALSE or trim($text) === '') {
 		$text = "document.title";
+	} else {
+		$text = "'" . htmlspecialchars(addcslashes($text, "'")) . "'";
 	}
-	else
-	{
-		$text = "'". htmlspecialchars(addcslashes($text, "'")). "'";
-	}
-	
-	if ($short === TRUE)
-	{
+
+	if ($short === TRUE) {
 		$btn_img = 'http://i.yimg.jp/images/sicons/ybm16.gif';
 		$btn_width = '16';
-		$btn_heigth = '16';
-	}
-	else
-	{
+		$btn_height = '16';
+	} else {
 		$btn_img = 'http://i.yimg.jp/images/ybm/blogparts/addmy_btn.gif';
 		$btn_width = '125';
-		$btn_heigth = '17';
+		$btn_height = '17';
 	}
-	
-	if ($nowindow === TRUE)
-	{
+
+	if ($nowindow === TRUE) {
 		$jscript = <<<EOS
 <a href="javascript:location.href='http://bookmarks.yahoo.co.jp/action/bookmark?t='+encodeURIComponent({$text})+'&amp;u='+encodeURIComponent(location.href);"><img src="{$btn_img}" width="{$btn_width}" height="{$btn_height}" alt="Yahoo!ブックマークに登録" style="border:none;"></a>
 EOS;
-	}
-	else
-	{
+	} else {
 		$jscript = <<<EOS
 <a href="javascript:void window.open('http://bookmarks.yahoo.co.jp/bookmarklet/showpopup?t='+encodeURIComponent({$text})+'&amp;u='+encodeURIComponent(location.href)+'&amp;ei=UTF-8','_blank','width=550,height=480,left=100,top=50,scrollbars=1,resizable=1',0);"><img src="{$btn_img}" width="{$btn_width}" height="{$btn_height}" alt="Yahoo!ブックマークに登録" style="border:none;"></a>
 EOS;
